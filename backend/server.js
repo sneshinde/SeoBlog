bc6d7008d553a5f0834f8ecdae2da3eb79ef2cdd
sneshinde <42643006+sneshinde@users.noreply.github.com>
2020-09-20 20:55:00 -0700

    heroku deployment steps

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
 const tagRoutes = require('./routes/tag');
 const formRoutes = require('./routes/form');
-
+const path = require('path');
 const mongoose = require('mongoose');
 // import express from 'express';
 // import cors from 'cors';
@@ -24,7 +24,7 @@ const app = express();
 
 //mongodb
 mongoose
-    .connect(process.env.DATABASE_LOCAL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+    .connect(process.env.MONGODB_URI || process.env.DATABASE_LOCAL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
     .then(() => console.log('MongoDB Connected'));
 
 //middlewares
@@ -49,6 +49,15 @@ app.use('/api', formRoutes);
     res.json({time: Date().toString()});
 });*/
 
+//Serve static assets if in production
+if(process.env.NODE_ENV == 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 const port = process.env.PORT || 8000;
 
 app.listen(port, ()=>{
