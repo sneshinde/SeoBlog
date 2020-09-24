c7c2156d26d68d7b99dbe318e094525f400c9ee1
sneshinde <42643006+sneshinde@users.noreply.github.com>
2020-09-24 12:29:41 -0700

    commit

--- frontend/config.js
+++ frontend/config.js
@@ -11,6 +11,6 @@ export const DOMAIN = publicRuntimeConfig.PRODUCTION
 
 export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
 export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
-export const GOOGLE_CLIENT_ID = publicRuntimeConfig.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_ID = publicRuntimeConfig.GOOGLE_CLIENT_ID;  
 
 
